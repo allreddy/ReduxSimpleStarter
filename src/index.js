4b7de6b103fa2aaaa19dd5e1ac788e6a33b2c149
allreddy <nithin@enplug.com>
2018-11-06 22:08:39 -0800

    introduce and import SearchBar as a class based component into App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar.js';
+
+const API_KEY = 'AIzaSyDyVKmze_hiG4R1Y4BaQh3FjjKFjZRY2Ug';
+
 // Create a new Component. This component should produce some HTML.
-const App = function() {
-    return <div>Hi!</div>;
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 
 // Take this Component's generated HTML and put it on the page(in the DOM).
-
+ReactDOM.render(<App />, document.querySelector('.container'));
